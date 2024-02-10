657ed2aa90487ef73df9be7ace0af0ccaf67d864
EC2 Default User <ec2-user@ip-172-31-14-130.ap-northeast-1.compute.internal>
2024-02-10 05:29:11 +0000

    一旦、userのeditとupdateの部分を実装した。ここで気付いたのが＠user.idとcurrent_userは同じだということだった。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  
   def index
     @users = User.all
   end
@@ -8,5 +9,25 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    end
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(current_user), notice: 'You have created user successfully.'
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+  
 end
